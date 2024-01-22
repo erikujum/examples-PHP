91882903d5eaf780219626299d2967500c35801d
erikujum <erik.vasquez@aulavirtual.umar.mx>
2024-01-22 11:00:08 -0600

    version 1
    version 1.1

---
+++ code/laravel/proyecto_0/laravel/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto 0</title>
+</head>
+<body>
+    <h2>Hola laravel 👽</h2>
+</body>
+</html>
