6ae6d81db4d6df83a8ff8dec45630b628e4143f4
erikujum <erik.vasquez@aulavirtual.umar.mx>
2024-01-22 15:13:48 -0600

    actuzalizacion pequeña
    mmm

--- code/laravel/proyecto_0/laravel/resources/views/app.blade.php
+++ code/laravel/proyecto_0/laravel/resources/views/app.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Be present above all else. - Naval Ravikant -->
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+    <!-- Fonts !-->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
+<!--<div>
+</div>!-->
--- code/laravel/proyecto_0/laravel/resources/views/login.blade.php
+++ code/laravel/proyecto_0/laravel/resources/views/login.blade.php
@@ -1,3 +1,20 @@
 <div>
-    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+    @extends('app')
+    @section('content')
+    @if(session('success'))
+        <h1>{{session('success')}}</h1>
+    @endif
+
+    <main class="login-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center"> Formulario login </h3>
+                        <div class="card-body"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </div>
