8785486be33fdca83378edbb82acd225a531819c
erikujum <erik.vasquez@aulavirtual.umar.mx>
2024-01-22 20:50:48 -0600

    version final
    version final

--- code/laravel/proyecto_0/laravel/resources/views/app.blade.php
+++ code/laravel/proyecto_0/laravel/resources/views/app.blade.php
@@ -2,10 +2,12 @@
 <html lang="{{ str_replace('_','-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <title>Laravel</title>
+
     <!-- Fonts !-->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <style>
         body {
@@ -17,5 +19,3 @@
     @yield('content')
 </body>
 </html>
-<!--<div>
-</div>!-->
--- code/laravel/proyecto_0/laravel/resources/views/main.blade.php
+++ code/laravel/proyecto_0/laravel/resources/views/main.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    {{-- Scripts --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <style type="text/css">
+        i{
+            font-size: 50px;
+        }
+    </style>
+</head>
+<body>
+    <div id="app">
+        <main class="container">
+            <h1> Como instalar Bootstrap en Laravel 10</h1>
+            <div class="card">
+                <div class="card-header">
+                    Icons
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
